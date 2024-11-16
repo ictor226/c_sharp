3f1157b756366ae3ef8a4aba47bb11e42270a52d
Victor Huggo <167533745+ictor226@users.noreply.github.com>
2024-11-16 11:01:56 -0300

    Add files via upload

---
+++ projeto01/projeto01/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto01
+{
+	internal class contaBancaria
+	{
+		private int numero_da_conta;
+		private string Titular_da_conta;
+		private double saldo_da_conta;
+
+        public double Saldo
+        {
+            get { return saldo_da_conta; }
+            set {  saldo_da_conta = value;}
+        }
+
+        public string titular_da_conta
+        {
+			get { return Titular_da_conta; }
+			set { Titular_da_conta = value; }
+		}
+      
+
+        public int Numero_da_conta
+        {
+            get { return numero_da_conta; }
+            set { numero_da_conta = value; }
+        }
+
+
+        //construtures
+        public contaBancaria(int numero_da_conta, string titular_da_conta)
+        {
+            this.numero_da_conta = numero_da_conta;
+            Titular_da_conta = titular_da_conta;
+        }
+
+        public contaBancaria(int numero_da_conta, string titular_da_conta, double saldo_da_conta) : this(numero_da_conta, titular_da_conta)
+        {
+            this.saldo_da_conta = saldo_da_conta;
+        }
+
+        //metodos 
+         public void Deposito (double quantia)
+        {
+            saldo_da_conta += quantia;
+        }
+
+        public void Saque (double quantia)
+        {
+            saldo_da_conta -= quantia +5;
+        }
+    }
+}
---
+++ projeto01/projeto01/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using projeto01;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        contaBancaria conta;
+
+        Console.Write("entre com o numero da conta: ");
+        int n=int.Parse(Console.ReadLine());
+
+        Console.Write("entre com o nome do titular da conta: ");
+        string nome = Console.ReadLine();
+        Console.Write("deja fazer o deposito inicial (s/n): ");
+        char a = char.Parse(Console.ReadLine());
+
+        if (a == 's')
+        {
+            Console.Write("entre com o valor de deposito inicial: ");
+            double saldo = double.Parse(Console.ReadLine());
+            conta = new contaBancaria(n, nome, saldo);
+            
+            Console.WriteLine("dados da conta: ");
+            Console.WriteLine($"\t conta: {conta.Numero_da_conta}" +
+                $" titular: {conta.titular_da_conta} Saldo: {conta.Saldo}");
+        }
+
+        else
+        {
+            conta = new contaBancaria (n, nome);
+            Console.WriteLine("dados da conta: ");
+            Console.WriteLine($"\t conta: {conta.Numero_da_conta}" +
+                $" titular: {conta.titular_da_conta} Saldo: {conta.Saldo}");
+        }
+
+        Console.WriteLine("entre com um valor para o  deposito");
+        double valor = double.Parse(Console.ReadLine());    
+        
+        
+        conta.Deposito(valor);
+        Console.WriteLine("dados da conta atualizados: ");
+        Console.WriteLine($"\t conta: {conta.Numero_da_conta}" +
+            $" titular: {conta.titular_da_conta} Saldo: {conta.Saldo}");
+
+
+        Console.WriteLine("entre com um valor para saque: ");
+        valor = double.Parse(Console.ReadLine());
+        conta.Saque(valor);
+
+        Console.WriteLine("dados da atualizados: ");
+        Console.WriteLine($"\t conta: {conta.Numero_da_conta}" +
+            $" titular: {conta.titular_da_conta} Saldo: {conta.Saldo}");
+
+    }
+}
